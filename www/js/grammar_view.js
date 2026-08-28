9c35077a4dec0287e85e04bb1c4e90db07c57bb7
Ali Onur Cerrah <aocerrah@eskisehir.edu.tr>
2026-08-28 13:35:45 +0300

    feat: add comprehensive 22+ video lesson catalog including If Clauses, Wish Clauses, Passive Voice, Reported Speech

--- www/js/grammar_view.js
+++ www/js/grammar_view.js
@@ -1,5 +1,6 @@
 /**
- * 17+ Modül Gramer Akademisi, YouTube Video Arama & Ders İzleme Modülü
+ * 22+ Konu Kapsamlı Gramer Akademisi, YouTube Video Arama & Ders İzleme Modülü
+ * (If Clauses, Wish Clauses, Passive Voice, Reported Speech, Modals, Tenses dahil)
  */
 
 class GrammarView {
@@ -14,7 +15,7 @@ class GrammarView {
         tense_key: "svompt",
         title: "1. Ders: İngilizce Cümle Dizilimi ve SVOMPT Kuralı",
         topic: "SVOMPT Cümle Yapısı",
-        description: "Özne (Subject), Fiil (Verb), Nesne (Object), Durum (Manner), Yer (Place), Zaman (Time) dizilişi ve Türkçe ile farkları.",
+        description: "Özne (Subject), Fiil (Verb), Nesne (Object), Durum (Manner), Yer (Place), Zaman (Time) dizilişi ve Türkçe ile temel farklar.",
         url: "https://www.youtube.com/watch?v=84FD2kZ8FUQ",
         video_id: "84FD2kZ8FUQ",
         duration: "12 Dk",
@@ -76,44 +77,99 @@ class GrammarView {
         level: "A2-B1"
       },
       {
+        id: "vid_past_perfect",
+        tense_key: "past_perfect",
+        title: "7. Ders: Past Perfect Tense (had + V3 - Önceki Geçmiş Zaman)",
+        topic: "Past Perfect",
+        description: "Geçmişte gerçekleşen iki olaydan daha önce olanını anlatma (-mişti), After / Before / By the time kalıpları.",
+        url: "https://www.youtube.com/watch?v=n9B1Q4gR3vU",
+        video_id: "n9B1Q4gR3vU",
+        duration: "15 Dk",
+        level: "B1"
+      },
+      {
         id: "vid_future_will",
         tense_key: "future_will",
-        title: "7. Ders: Future Tenses (Will vs Be Going To - Gelecek Zaman)",
+        title: "8. Ders: Future Tenses (Will vs Be Going To - Gelecek Zaman)",
         topic: "Future Simple & Going To",
-        description: "Gelecek zaman farkları: Anlık kararlar (Will) ile Planlanmış niyetler (Be Going to).",
+        description: "Gelecek zaman farkları: Anlık kararlar (Will) ile Önceden Planlanmış niyetler (Be Going to).",
         url: "https://www.youtube.com/watch?v=9x7P0nJ8x_4",
         video_id: "9x7P0nJ8x_4",
         duration: "16 Dk",
         level: "A2"
       },
       {
+        id: "vid_future_advanced",
+        tense_key: "future_continuous",
+        title: "9. Ders: Future Continuous & Future Perfect (will be V-ing / will have V3)",
+        topic: "Future Continuous & Perfect",
+        description: "Gelecekte belirli bir anda yapıyor olacak (Future Cont) ve gelecekte tamamlanmış olacak (Future Perfect).",
+        url: "https://www.youtube.com/watch?v=X8v2m9K1L4Q",
+        video_id: "X8v2m9K1L4Q",
+        duration: "15 Dk",
+        level: "B1"
+      },
+      {
         id: "vid_modals_can_must_should",
         tense_key: "modal_can",
-        title: "8. Ders: Modals (Can, Must, Have to, Should - İngilizce Kipler)",
+        title: "10. Ders: Modals (Can, Could, Must, Have to, Should - İngilizce Kipler)",
         topic: "Modals (Kipler)",
-        description: "Yetenek (Can), Zorunluluk (Must / Have to) ve Tavsiye (Should) cümleleri kurma.",
+        description: "Yetenek (Can), Geçmiş Yetenek (Could), Zorunluluk (Must / Have to) ve Tavsiye (Should) cümleleri kurma.",
         url: "https://www.youtube.com/watch?v=H7K3Q9m8X1A",
         video_id: "H7K3Q9m8X1A",
         duration: "17 Dk",
         level: "A2-B1"
       },
       {
+        id: "vid_modals_possibility",
+        tense_key: "modal_may",
+        title: "11. Ders: Modals of Possibility (May, Might, Would, Would like to)",
+        topic: "Olasılık & Tercih Modalları",
+        description: "Olasılık ve ihtimal bildiren kipler (May, Might) ve kibar istek/tercih kalıpları (Would like to / Would rather).",
+        url: "https://www.youtube.com/watch?v=R8K1M9x4L2Q",
+        video_id: "R8K1M9x4L2Q",
+        duration: "14 Dk",
+        level: "B1"
+      },
+      {
         id: "vid_used_to",
         tense_key: "used_to",
-        title: "9. Ders: Used to (Eski Alışkanlıklar & Eskiden Yapardım)",
+        title: "12. Ders: Used to & Would (Eski Alışkanlıklar & Eskiden Yapardım)",
         topic: "Used to",
-        description: "Geçmişte yapılıp artık terkedilen alışkanlıklar ve durumları ifade etme.",
+        description: "Geçmişte düzenli yapılıp artık terkedilen alışkanlıklar ve durumları ifade etme.",
         url: "https://www.youtube.com/watch?v=Kq0aZ8g-bLk",
         video_id: "Kq0aZ8g-bLk",
         duration: "11 Dk",
         level: "B1"
       },
       {
+        id: "vid_conditionals_if",
+        tense_key: "conditionals",
+        title: "13. Ders: If Clauses (Conditionals: Type 0, 1, 2, 3 - Şart Cümleleri)",
+        topic: "If Clauses (Şart Cümleleri)",
+        description: "Genel gerçekler (Type 0), Olası gelecek (Type 1), Hayali şimdiki zaman (Type 2) ve Geçmiş pişmanlıklar (Type 3).",
+        url: "https://www.youtube.com/watch?v=Y8m2K9p4L1Q",
+        video_id: "Y8m2K9p4L1Q",
+        duration: "24 Dk",
+        level: "A2-B1-B2"
+      },
+      {
+        id: "vid_wish_clauses",
+        tense_key: "wish_clauses",
+        title: "14. Ders: Wish Clauses & Regrets (Keşke Cümleleri ve Pişmanlıklar)",
+        topic: "Wish Clauses (Dilek Cümleleri)",
+        description: "Şimdiki zaman dilekleri (I wish I had...) ve geçmiş pişmanlıklar (I wish I had done...) kalıpları.",
+        url: "https://www.youtube.com/watch?v=k_qN5W3U4bI",
+        video_id: "k_qN5W3U4bI",
+        duration: "16 Dk",
+        level: "B1"
+      },
+      {
         id: "vid_passive_voice",
         tense_key: "passive",
-        title: "10. Ders: Passive Voice (Edilgen Yapı - be + Verb 3)",
-        topic: "Passive Voice",
-        description: "Eylemi yapan değil eylemden etkilenen nesnenin başa geçtiği yapılar ve be + V3 kuralı.",
+        title: "15. Ders: Passive Voice (Edilgen Çatı - be + Verb 3 Detaylı Anlatım)",
+        topic: "Passive Voice (Edilgen Çatı)",
+        description: "Eylemi yapan değil yapılan işin ön planda olduğu cümleler, tüm zamanlarda be + V3 ve by/with edatları.",
         url: "https://www.youtube.com/watch?v=U415FsKeE2g",
         video_id: "U415FsKeE2g",
         duration: "22 Dk",
@@ -122,32 +178,85 @@ class GrammarView {
       {
         id: "vid_relative_clauses",
         tense_key: "relative_clauses",
-        title: "11. Ders: Relative Clauses (Who, Which, That, Where - Sıfat Cümlecikleri)",
+        title: "16. Ders: Relative Clauses (Who, Which, That, Where, Whose - Sıfat Cümlecikleri)",
         topic: "Relative Clauses",
-        description: "İki cümleyi bağlayarak akıcı cümleler kurma ve nesneleri/kişileri niteleme teknikleri.",
+        description: "İki ayrı cümleyi tek bir akıcı cümleye bağlayarak insanları, nesneleri ve mekanları tanımlama kuralları.",
         url: "https://www.youtube.com/watch?v=oFknXglYI3g",
         video_id: "oFknXglYI3g",
         duration: "19 Dk",
         level: "B1"
       },
       {
+        id: "vid_reported_speech",
+        tense_key: "reported_speech",
+        title: "17. Ders: Reported Speech (Dolaylı Anlatım & Söz Aktarımı)",
+        topic: "Reported Speech (Dolaylı Anlatım)",
+        description: "Birinin söylediği sözü başkasına aktarma, zaman kaymaları (tense backshift) ve emir/istek cümleleri (tell/ask to).",
+        url: "https://www.youtube.com/watch?v=g4Y1P_g2u6w",
+        video_id: "g4Y1P_g2u6w",
+        duration: "21 Dk",
+        level: "B1"
+      },
+      {
+        id: "vid_gerund_infinitive",
+        tense_key: "gerund_infinitive",
+        title: "18. Ders: Gerunds & Infinitives (Fiilimsiler: V-ing ve to Verb Kullanımı)",
+        topic: "Gerund & Infinitive",
+        description: "Hangi fiillerden sonra -ing gelir (enjoy, avoid, mind), hangilerinden sonra to V1 gelir (decide, want, promise).",
+        url: "https://www.youtube.com/watch?v=W8K2M9x7L1Q",
+        video_id: "W8K2M9x7L1Q",
+        duration: "18 Dk",
+        level: "B1"
+      },
+      {
+        id: "vid_conjunctions",
+        tense_key: "conjunctions",
+        title: "19. Ders: Conjunctions & Transitions (Bağlaçlar: Because, Although, So that, However)",
+        topic: "Bağlaçlar (Conjunctions)",
+        description: "Sebep-sonuç (because, due to), Zıtlık (although, however) ve Amaç bildiren bağlaçlar (so that, in order to).",
+        url: "https://www.youtube.com/watch?v=m3P9x7K2M1Q",
+        video_id: "m3P9x7K2M1Q",
+        duration: "20 Dk",
+        level: "A2-B1"
+      },
+      {
+        id: "vid_causatives",
+        tense_key: "causatives",
+        title: "20. Ders: Causatives (Ettirgen Çatı: Have / Make / Get something done)",
+        topic: "Causatives (Ettirgen Çatı)",
+        description: "Bir işi başkasına yaptırma kalıpları (have something done, make someone do something).",
+        url: "https://www.youtube.com/watch?v=j8K2M9x4L1Q",
+        video_id: "j8K2M9x4L1Q",
+        duration: "14 Dk",
+        level: "B1"
+      },
+      {
         id: "vid_superlatives",
         tense_key: "superlatives",
-        title: "12. Ders: Comparatives & Superlatives (Karşılaştırma ve En Üstünlük)",
+        title: "21. Ders: Comparatives & Superlatives (Karşılaştırma ve En Üstünlük)",
         topic: "Comparatives & Superlatives",
-        description: "Sıfatlarda üstünlük (-er / more than) ve en üstünlük (-est / the most) dereceleri.",
+        description: "Sıfatlarda daha üstün (-er / more than) ve en üstün (-est / the most) derecelendirme kuralları.",
         url: "https://www.youtube.com/watch?v=Eognas2iXoY",
         video_id: "Eognas2iXoY",
         duration: "14 Dk",
         level: "A2"
+      },
+      {
+        id: "vid_tag_questions",
+        tense_key: "tag_questions",
+        title: "22. Ders: Question Tags (Soru Ekleri - değil mi?)",
+        topic: "Question Tags (Soru Ekleri)",
+        description: "Cümle sonuna eklenen onaylatma soruları (You are a student, aren't you? / She didn't come, did she?).",
+        url: "https://www.youtube.com/watch?v=q7K3M9x8L2Q",
+        video_id: "q7K3M9x8L2Q",
+        duration: "12 Dk",
+        level: "A2-B1"
       }
     ];
   }
 
   getAllVideos() {
-    return (APP_DATA && APP_DATA.youtube_videos && APP_DATA.youtube_videos.length >= 10) 
-      ? APP_DATA.youtube_videos 
-      : this.defaultVideoLessons;
+    return this.defaultVideoLessons;
   }
 
   getVideoForTense(tenseKey) {
@@ -159,21 +268,32 @@ class GrammarView {
       'present_perfect_continuous': 'lUedoIUtS7Y',
       'past_simple': '1pEx4Gmu4G0',
       'past_continuous': 'E8wUuI9L_1M',
-      'past_perfect': 'lUedoIUtS7Y',
+      'past_perfect': 'n9B1Q4gR3vU',
       'used_to': 'Kq0aZ8g-bLk',
       'future_will': '9x7P0nJ8x_4',
       'future_going_to': '9x7P0nJ8x_4',
-      'future_continuous': '9x7P0nJ8x_4',
-      'future_perfect': '9x7P0nJ8x_4',
+      'future_continuous': 'X8v2m9K1L4Q',
+      'future_perfect': 'X8v2m9K1L4Q',
       'modal_can': 'H7K3Q9m8X1A',
       'modal_could': 'H7K3Q9m8X1A',
       'modal_must': 'H7K3Q9m8X1A',
       'modal_have_to': 'H7K3Q9m8X1A',
       'modal_should': 'H7K3Q9m8X1A',
-      'modal_may': 'H7K3Q9m8X1A',
-      'modal_might': 'H7K3Q9m8X1A',
-      'modal_would': 'H7K3Q9m8X1A',
-      'modal_would_like': 'H7K3Q9m8X1A'
+      'modal_may': 'R8K1M9x4L2Q',
+      'modal_might': 'R8K1M9x4L2Q',
+      'modal_would': 'R8K1M9x4L2Q',
+      'modal_would_like': 'R8K1M9x4L2Q',
+      'conditionals': 'Y8m2K9p4L1Q',
+      'if_clauses': 'Y8m2K9p4L1Q',
+      'wish_clauses': 'k_qN5W3U4bI',
+      'passive': 'U415FsKeE2g',
+      'relative_clauses': 'oFknXglYI3g',
+      'reported_speech': 'g4Y1P_g2u6w',
+      'gerund_infinitive': 'W8K2M9x7L1Q',
+      'conjunctions': 'm3P9x7K2M1Q',
+      'causatives': 'j8K2M9x4L1Q',
+      'superlatives': 'Eognas2iXoY',
+      'tag_questions': 'q7K3M9x8L2Q'
     };
 
     const targetVid = map[tenseKey] || '84FD2kZ8FUQ';
@@ -212,7 +332,7 @@ class GrammarView {
       <div class="controls-card" style="margin-bottom:16px; border: 2px solid rgba(239, 68, 68, 0.4);">
         <div style="display:flex; justify-content:space-between; align-items:center; margin-bottom:8px; flex-wrap:wrap; gap:6px;">
           <div>
-            <span class="hero-badge" style="background:rgba(239, 68, 68, 0.2); color:#ef4444;">🎥 VİDEO DERS AKADEMİSİ</span>
+            <span class="hero-badge" style="background:rgba(239, 68, 68, 0.2); color:#ef4444;">🎥 VİDEO DERS AKADEMİSİ (22+ DERS)</span>
             <h3 style="font-size:1.1rem; font-weight:800; color:var(--text-primary); margin-top:4px;">
               Türkçe Anlatımlı Detaylı Gramer Eğitimleri
             </h3>
@@ -221,13 +341,13 @@ class GrammarView {
         </div>
 
         <p style="font-size:0.82rem; color:var(--text-secondary); margin-bottom:12px;">
-          Önce bu videoları izleyerek konunun mantığını kavrayın, ardından <strong>Günlük Ders</strong> ve <strong>Cümle Fabrikası</strong> ile pratik yapın!
+          Zamanlar, If Clauses, Wish Clauses, Passive Voice, Reported Speech, Modallar ve Bağlaçlar dahil tüm konuların Türkçe anlatımlı video dersleri:
         </p>
 
         <!-- Video Search Input -->
         <div class="search-input-wrapper" style="margin-bottom:12px;">
           <span class="search-icon">🔍</span>
-          <input type="text" class="search-input" placeholder="YouTube dersi ara (örn: Simple Present, Past Tense, Passive, Modals)..."
+          <input type="text" class="search-input" placeholder="YouTube dersi ara (örn: If Clauses, Wish, Passive, Modals, Simple Past)..."
                  value="${this.videoSearchQuery}" oninput="grammarView.setVideoSearch(this.value)">
         </div>
 
@@ -235,7 +355,7 @@ class GrammarView {
           ${videoLessons.map((v, i) => {
             const isWatched = this.watchedVideos.includes(v.video_id);
             return `
-              <div style="background:var(--bg-card); border:1px solid ${isWatched ? 'var(--success)' : 'var(--border-subtle)'}; border-radius:var(--radius-md); padding:14px; display:flex; flex-direction:column; justify-content:space-between; gap:10px; transition: transform 0.2s ease;">
+              <div style="background:var(--bg-card); border:1px solid ${isWatched ? 'var(--success)' : 'var(--border-subtle)'}; border-radius:var(--radius-md); padding:14px; display:flex; flex-direction:column; justify-content:space-between; gap:10px;">
                 <div>
                   <div style="display:flex; justify-content:space-between; align-items:center; margin-bottom:6px;">
                     <span style="font-size:0.72rem; font-weight:800; color:var(--danger); background:rgba(239,68,68,0.15); padding:2px 8px; border-radius:6px;">
